6286e6632a292e9d4ff21a8b407a5d20a5e8fefc
Muhammad Basim Irfan <mbasimirfan65@gmail.com>
2026-04-10 18:39:26 +0500

    web pages setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css'
+import './App.css';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Profile from './pages/Profile';
+import Contacts from './pages/Contact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/' element={<Layout />}>
+
+            <Route index element={<Home />} />
+
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
